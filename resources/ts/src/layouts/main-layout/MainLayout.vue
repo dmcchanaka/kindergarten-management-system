f9ba6359c2eb97579ded980e34124cc2cbd150c1
chanaka <chanaka@itplace.com>
2023-09-14 10:32:18 +0530

    change layout

--- resources/ts/src/layouts/main-layout/MainLayout.vue
+++ resources/ts/src/layouts/main-layout/MainLayout.vue
@@ -3,8 +3,28 @@
     <Aside :class="{ 'translate-x-0': isSidebarOpen, 'shadow-soft-xl': isSidebarOpen }" :style="{ 'margin-left': isSidebarOpen ? null : '0rem' }"></Aside>
     <main class="ease-soft-in-out xl:ml-68.5 relative h-full max-h-screen rounded-xl transition-all duration-200 mt-4">
       <Topbar @sidebarToggle="sidebarToggle"></Topbar>
-
+      
+      
       <div class="w-full px-6 py-6 mx-auto">
+        <div class="lg:flex lg:items-center lg:justify-between">
+          <div class="flex-1 min-w-0">
+            <nav>
+              <!-- breadcrumb -->
+              <ol class="flex flex-wrap pt-1 mr-12 bg-transparent rounded-lg sm:mr-16">
+                <li class="leading-normal text-sm">
+                  <router-link to="/dashboard" class="text-slate-700 text-sm"><fa icon="home" class="h-3 w-4 text-sm mb-0.5" />&nbsp;Home</router-link>
+                </li>
+                <template v-for="(item, i) in breadcrumbs" :key="i">
+                  <li class="text-sm pl-2 capitalize leading-normal text-slate-700 before:float-left before:pr-2 before:text-gray-600 before:content-['/']" aria-current="page">{{ item }}</li>
+                </template>
+              </ol>
+              <h5 class="mb-0 font-bold capitalize">{{ pageTitle }}</h5>
+            </nav>
+          </div>
+          <div class="flex mt-5 lg:ml-4 lg:mt-0">
+
+          </div>
+        </div>
         <router-view></router-view>
         <!-- Footer -->
       <Footer></Footer>
@@ -20,6 +40,7 @@
 
 <script lang="ts">
 import { defineComponent, computed, ref } from "vue";
+import { useRoute } from "vue-router";
 
 import Aside from "@/layouts/main-layout/aside/Aside.vue";
 import Topbar from "@/layouts/main-layout/header/TopBar.vue";
@@ -35,6 +56,7 @@ export default defineComponent({
     Footer
   },
   setup() {
+    const route = useRoute();
     const store = useSettingsStore();
 
     const backgroundColor = computed(()=> {
@@ -47,10 +69,20 @@ export default defineComponent({
       isSidebarOpen.value = !isSidebarOpen.value;
     }
 
+    const pageTitle = computed(() => {
+      return route.meta.pageTitle;
+    });
+
+    const breadcrumbs = computed(() => {
+      return route.meta.breadcrumbs;
+    });
+
     return {
       backgroundColor,
       sidebarToggle,
-      isSidebarOpen
+      isSidebarOpen,
+      pageTitle,
+      breadcrumbs
     }
   },
 });
